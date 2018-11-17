a25557c90e938c77d229727e6ed7e31bbb536b48
jusin-PC\Just <justincheung6@gmail.com>
2018-11-17 04:59:42 -0800

    Got other two types of AI working and made prefabs for them all. Also made Tags and Layers

--- Fall18_24H_Slam_Jam/Assets/_Script/EnemyAI_Magenta.cs
+++ Fall18_24H_Slam_Jam/Assets/_Script/EnemyAI_Magenta.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI_Red : MonoBehaviour {
+public class EnemyAI_Magenta : MonoBehaviour {
 
-    private Transform tran_EnemyR;
+    private Transform tran_EnemyM;
     public Transform tran_Player;
-    public float speed_EnemyR = 8;
-    public float targetOffset_Enemy = 30;
+    public float speed_EnemyM = 8;
+    public float targetOffset_EnemyM = 30;
 
 	void Awake () {
         GameObject Player = GameObject.Find("Player");
         tran_Player = Player.GetComponent<Transform>();
-        tran_EnemyR = GetComponent<Transform>();
+        tran_EnemyM = GetComponent<Transform>();
     }
 	
 	void Update () {
     /*    Movement    */
-        tran_EnemyR.Translate(Vector3.right * Time.deltaTime * speed_EnemyR);
+        tran_EnemyM.Translate(Vector3.right * Time.deltaTime * speed_EnemyM);
     }
 
     private void OnCollisionStay2D(Collision2D col)
@@ -28,7 +28,7 @@ public class EnemyAI_Red : MonoBehaviour {
             Vector3 dir = tran_Player.position - transform.position;
             float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-            transform.eulerAngles = transform.eulerAngles + new Vector3(0,0,(Random.Range(-1, 1) * targetOffset_Enemy));
+            transform.eulerAngles = transform.eulerAngles + new Vector3(0,0,(Random.Range(-1, 1) * targetOffset_EnemyM));
         }
     }
 }
---
+++ Fall18_24H_Slam_Jam/Assets/_Script/EnemyAI_Yellow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI_Yellow : MonoBehaviour {
+
+    private Transform tran_EnemyY;
+    public Transform tran_Player;
+    public float speed_EnemyY = 1;
+
+    void Awake()
+    {
+        GameObject Player = GameObject.Find("Player");
+        tran_Player = Player.GetComponent<Transform>();
+        tran_EnemyY = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        /*    Movement    */
+        tran_EnemyY.Translate(Vector3.right * Time.deltaTime * speed_EnemyY);
+
+        /*    Look at Player    */
+        Vector3 dir = tran_Player.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.eulerAngles = transform.eulerAngles;
+    }
+
+}
