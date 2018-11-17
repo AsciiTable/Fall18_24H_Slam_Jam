e9576b4e4a7d6c915e234c6455c82e0511629292
mangouyu <sagwa365nam@aol.com>
2018-11-17 00:22:20 -0800

    my first bby

---
+++ Fall18_24H_Slam_Jam/Assets/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+	public float fireRate = 0;
+	public float Damage = 10;
+	public LayerMask notToHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Fall18_24H_Slam_Jam/Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public Rigidbody2D myRigid;
+
+	private float xMove;
+	private float yMove;
+	public float speed = 10;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerController();
+	}
+
+	void playerController()
+	{
+		//This will get the input of the player based
+		//on what key is pressed and how hard it is pressed
+		//it will then store the value into xMove
+		xMove = Input.GetAxis("Horizontal")*speed;
+		yMove = Input.GetAxis("Vertical") * speed;
+
+		//This lets you access the velocity vector of the rigidbody 
+		//and give it values based on the buttons pressed (x and y values)
+		myRigid.velocity = new Vector2(xMove, yMove);
+		
+
+	}
+}
