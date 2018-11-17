ba8718bbe4b6c51c58da05f5eac3480341f46e29
mangouyu <sagwa365nam@aol.com>
2018-11-17 02:34:50 -0800

    shoot my foot then my face

---
+++ Fall18_24H_Slam_Jam/Assets/_Script/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+	public float fireRate = 0;
+	public float Damage = 10;
+	public LayerMask notToHit;
+
+	private float timeToFire = 0;
+	private Transform firePoint;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		firePoint = transform.FindChild("FirePoint");
+		if (firePoint == null)
+		{
+			Debug.LogError("No firepoint? NANIII??");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fireRate == 0)
+		{
+			if (Input.GetButtonDown("Fire1"))
+			{
+				Shoot();
+			}
+		}
+		else
+		{
+			if (Input.GetButtonDown("Fire1") && Time.time > timeToFire)
+			{
+				timeToFire = Time.time + 1 / fireRate;
+				Shoot();
+			}
+		}
+	}
+
+	void Shoot()
+	{
+		Debug.Log("Test");
+	}
+}
