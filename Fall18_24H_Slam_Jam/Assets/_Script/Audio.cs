44fcf8e23b0d5867d8ec603bba20dc93a0ab20e7
Jessica Wei <jessicawei9@gmail.com>
2018-11-17 08:13:02 -0800

    basic audio code initialized

---
+++ Fall18_24H_Slam_Jam/Assets/_Script/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+
+	public AudioClip JumpSFX;
+
+	public AudioSource MusicSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+		MusicSource.clip = JumpSFX;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			MusicSource.Play();
+		}
+	}
+}
